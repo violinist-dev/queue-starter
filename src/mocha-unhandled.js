a8c785248989386bf3a4c500b7cf53e9876f1f27
Eirik S. Morland <eirik@morland.no>
2019-12-29 18:05:00 +0100

    Debug

---
+++ src/mocha-unhandled.js
@@ -0,0 +1,13 @@
+let unhandledRejectionExitCode = 0;
+
+process.on("unhandledRejection", (reason) => {
+	console.log("unhandled rejection:", reason);
+	unhandledRejectionExitCode = 1;
+	throw reason;
+});
+
+process.prependListener("exit", (code) => {
+	if (code === 0) {
+		process.exit(unhandledRejectionExitCode);
+	}
+});
