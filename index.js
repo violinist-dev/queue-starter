88c8e85cbea20af094cdf15c8e23634ce9f09961
Eirik Stanghelle Morland <eirik@morland.no>
2020-05-28 14:02:21 +0200

    Feat/pull interval (#62)
    * Pull in interval
    
    * Only queue pull every hour or so
    
    * Code standard
    
    * Try catch the docker pull

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const queue = require('queue')
+const Docker = require('dockerode')
+const docker = new Docker()
 const eventLoopStats = require('event-loop-stats')
 const ks = require('kill-switch')
 const bunyan = require('bunyan')
@@ -6,6 +8,8 @@ const log = bunyan.createLogger({ name: 'queue-starter' })
 const config = require('./config')
 const git = require('git-rev')
 const createJob = require('./built/createJob')
+const promisifyModule = require('./built/promisify')
+const promisify = promisifyModule.default
 const createCloudJob = require('./built/createCloudJob').createCloudJob
 var gitRev
 git.short(function (str) {
@@ -47,6 +51,50 @@ async function start () {
     q.start()
   }
 }
+
+function createPullJob (img) {
+  return async function () {
+    try {
+      log.info('Pulling img for ' + img, {
+        img
+      })
+      const startTime = Date.now()
+      const stream = await promisify(docker.pull.bind(docker, 'violinist/update-check-runner:' + img))
+      for await (const _ of stream) {
+        // Ignore this, but use the variable so standard does not complain.
+        let chunk = _
+        chunk = chunk.toString()
+      }
+      const pullTime = Date.now() - startTime
+      log.info('Pull finished for ' + img, {
+        pullTime,
+        img
+      })
+    } catch (err) {
+      log.error('There was an error: ', {
+        err
+      })
+    }
+  }
+}
+
+async function queuePull () {
+  const imgs = [
+    '7.0',
+    '7.1',
+    '7.2',
+    '7.3',
+    '7.4'
+  ]
+  const jobs = imgs.map(async (img) => {
+    q.push(createPullJob(img))
+    q.start()
+  })
+
+  await Promise.all(jobs)
+  setTimeout(queueMicrotask, (60 * 1000 * 60))
+}
+queuePull()
 
 start()
 
