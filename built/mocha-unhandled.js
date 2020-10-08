13e9d07e1d0fd1f6a43f96231f67686b8781fe06
Eirik Stanghelle Morland <eirik@morland.no>
2020-10-08 21:28:01 +0300

    Make sure we pull and prune all upcoming imgs (#66)
    * Make sure we pull and prune all upcoming imgs
    
    * Update things to be their own modules

--- built/mocha-unhandled.js
+++ built/mocha-unhandled.js
@@ -1,10 +1,10 @@
 var unhandledRejectionExitCode = 0;
-process.on("unhandledRejection", function (reason) {
-    console.log("unhandled rejection:", reason);
+process.on('unhandledRejection', function (reason) {
+    console.log('unhandled rejection:', reason);
     unhandledRejectionExitCode = 1;
     throw reason;
 });
-process.prependListener("exit", function (code) {
+process.prependListener('exit', function (code) {
     if (code === 0) {
         process.exit(unhandledRejectionExitCode);
     }
---
+++ src/createPruneJob.ts
@@ -0,0 +1,33 @@
+import promisify from './promisify'
+import * as Docker from 'dockerode'
+const bunyan = require('bunyan')
+const docker = new Docker()
+const log = bunyan.createLogger({ name: 'queue-starter' })
+
+function createPruneJob (img) {
+  return async function () {
+    try {
+      log.info({
+        img
+      }, 'Prune img for ' + img)
+      const startTime = Date.now()
+      const pruneData = await promisify(docker.pruneImages.bind(docker, {
+        label: 'violinist/update-check-runner:' + img,
+        dangling: true
+      }))
+      log.info({
+        pruneData,
+        img
+      }, 'Prune data')
+      const pruneTime = Date.now() - startTime
+      log.info({
+        pruneTime,
+        img
+      }, 'Prune finished for ' + img)
+    } catch (err) {
+      log.error(err, 'There was an error: ')
+    }
+  }
+}
+
+module.exports = createPruneJob
