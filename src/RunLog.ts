63ed2de81e7110a9db2877852c9100a968776889
Eirik S. Morland <eirik@morland.no>
2020-11-22 21:43:12 +0100

    Add task definition and name to runlogs

--- src/RunLog.ts
+++ src/RunLog.ts
@@ -4,7 +4,13 @@ const log = bunyan.createLogger({ name: 'queue-starter' })
 export class Runlog {
     log
     constructor (data) {
-      this.log = log.child({ job_id: data.job_id, slug: data.slug, php: data.php_version, cloud: !!data.cloud })
+      if (!data.taskDefinition) {
+        data.taskDefinition = ''
+      }
+      if (!data.name) {
+        data.name = ''
+      }
+      this.log = log.child({ job_id: data.job_id, slug: data.slug, php: data.php_version, cloud: !!data.cloud, taskName: data.name, taskDefinition: data.taskDefinition })
       this.log.info('Creating a run log')
     }
 }
