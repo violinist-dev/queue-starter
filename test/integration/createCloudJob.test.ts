0c29c358091e954a9cff0a6519753399ba515e4f
Eirik Stanghelle Morland <eirik@morland.no>
2021-10-31 08:27:31 +0100

    Move integration tests into integration test directory (#89)

---
+++ test/integration/createCloudJob.test.ts
@@ -0,0 +1,35 @@
+import * as should from 'should';
+import * as AWS from 'aws-sdk'
+import { createEcsTaskDefinition } from "../../src/createCloudJob";
+const composerVersions = [1, 2]
+const supportedVersions = require("../../src/supportedPhpVersions")
+
+
+describe('createCloudJob', () => {
+
+    it('Should totally have all the images as tasks in AWS', async () => {
+        const awsconfig = {
+            accessKeyId: process.env.CI_ACCESS_KEY_ID,
+            secretAccessKey: process.env.CI_SECRET_KEY,
+            region: process.env.CI_AWS_REGION,
+        }
+        const client = new AWS.ECS(awsconfig)
+        let tasks = await client.listTaskDefinitions().promise()
+        // Now go through all PHP versions and all composer versions, and see that we have
+        // a matching definition for them.
+        for (const delta in supportedVersions) {
+            for (const cDelta in composerVersions) {
+                const composerVersion = composerVersions[cDelta]
+                const php_version = supportedVersions[delta]
+                const taskName = createEcsTaskDefinition({
+                    php_version,
+                    composer_version: composerVersion
+                })
+                let matchingTasks = tasks.taskDefinitionArns.filter((item) => {
+                    return item.includes(taskName)
+                })
+                matchingTasks.length.should.not.equal(0)
+            }
+        }
+    })
+})
