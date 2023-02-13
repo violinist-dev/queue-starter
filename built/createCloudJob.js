4cdc4a4301349fe80d535a8d2998bb263ff3a394
Eirik S. Morland <eirik@morland.no>
2023-02-13 12:15:09 +0100

    Build js

--- built/createCloudJob.js
+++ built/createCloudJob.js
@@ -65,7 +65,7 @@ exports.createEcsTaskDefinition = createEcsTaskDefinition;
 var createCloudJob = function (config, job, gitRev) {
     return function runJob(callback) {
         return __awaiter(this, void 0, void 0, function () {
-            var logData, data, name, taskDefinition, runLog, awsconfig, env, ecsClient, watchClient, startTime, taskData, task, taskArn, arnParts, retries, events, list, logErr_1, totalTime, stdout, message, updateData, publisher, statusData, err_1;
+            var logData, data, name, taskDefinition, runLog, awsconfig, env, ecsClient, watchClient, startTime, taskData, task, taskArn, arnParts, retries, events, list, logErr_1, totalTime, stdout, message, updateData, publisher, statusData, err_1, message, updateData, publisher, statusData;
             return __generator(this, function (_a) {
                 switch (_a.label) {
                     case 0:
@@ -80,7 +80,7 @@ var createCloudJob = function (config, job, gitRev) {
                         runLog.log.info('Trying to start cloud job for ' + logData.slug);
                         _a.label = 1;
                     case 1:
-                        _a.trys.push([1, 11, , 12]);
+                        _a.trys.push([1, 11, , 13]);
                         awsconfig = {
                             accessKeyId: config.accessKeyId,
                             secretAccessKey: config.secretAccessKey,
@@ -194,14 +194,35 @@ var createCloudJob = function (config, job, gitRev) {
                         statusData = _a.sent();
                         runLog.log.info('Job complete request code: ' + statusData.statusCode);
                         callback();
-                        return [3 /*break*/, 12];
+                        return [3 /*break*/, 13];
                     case 11:
                         err_1 = _a.sent();
                         runLog.log.error(err_1, 'There was an error running a cloud task');
+                        message = {
+                            stdout: [
+                                JSON.stringify([{
+                                        message: 'There was an error completing the job task. The error message was: ' + err_1.message,
+                                        type: 'message'
+                                    }])
+                            ],
+                            stderr: ''
+                        };
+                        updateData = {
+                            jobId: data.job_id,
+                            token: config.token,
+                            message: message,
+                            // This field is actually not even used at the moment I think. That's too bad.
+                            set_state: 'failure'
+                        };
+                        publisher = new publisher_1.default(config);
+                        return [4 /*yield*/, promisify_1.default(publisher.publish.bind(publisher, updateData))];
+                    case 12:
+                        statusData = _a.sent();
+                        runLog.log.info('Job complete request code: ' + statusData.statusCode);
                         // We do not care if things go ok, since things are queued so many times anyway.
                         callback();
-                        return [3 /*break*/, 12];
-                    case 12: return [2 /*return*/];
+                        return [3 /*break*/, 13];
+                    case 13: return [2 /*return*/];
                 }
             });
         });
