a333d9bb5bb904492e761f9cfcad7d7f1b098fb2
Eirik Stanghelle Morland <eirik@morland.no>
2023-05-17 20:02:03 +0200

    Avoid piling up queues (#117)

--- index.js
+++ index.js
@@ -30,8 +30,12 @@ const findJob = require('./built/findJob')
 async function start () {
   const job = await findJob(log, config)
   if (!job || !job.data || !job.data.job_id) {
-    if (!q.length) {
-      setTimeout(start, 60000)
+    if (!q.length && !cloudQueue.length) {
+      await sleep(60000)
+      // If we are now running something, we don't need to restart.
+      if (!q.length && !cloudQueue.length) {
+        start()
+      }
     } else {
       log.info('It seems we already have a something in the queue, trusting job search to be coming up')
     }
