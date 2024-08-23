836c65fc94b6f01219930ce031164a745c4fd0e4
Eirik Stanghelle Morland <eirik@morland.no>
2024-08-23 17:47:15 +0200

    Update createjob.test.ts

--- test/createjob.test.ts
+++ test/createjob.test.ts
@@ -26,7 +26,7 @@ describe('createJob', () => {
             should(fakeRunlog.Runlog.getCalls().error.length).not.equal(0)
             let hasErrorMessage = false
             fakeRunlog.Runlog.getCalls().warnings.forEach((logItem) => {
-                console.log(logItem[1].stdout)
+                logItem[1] && console.log(logItem[1].stdout)
                 if (logItem[0] === 'Status code was not 0, it was: 1')  {
                     hasErrorMessage = true
                 }
